45f2a880284ed41eea9b6b36dcb39b50135bbc52
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-10-28 09:13:51 +0300

    Initial commit

---
+++ USERS_DATABASE.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz
+{
+    public class USERS_DATABASE
+    {
+        private USERS_DATABASE() { }
+
+        private static List<User> users;
+        public static List<User> Users
+        {
+            get
+            {
+                if (users == null)
+                    users = new List<User>();
+                return users;
+            }
+            set
+            {
+                users = value;
+            }
+        }
+
+
+
+
+    }
+}
