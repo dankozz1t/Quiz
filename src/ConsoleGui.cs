835915ab19c807774c7963dbe74c0808a68e5e0b
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-10-31 13:51:50 +0200

    console beauty

---
+++ src/ConsoleGui.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace QuizGame
+{
+    public static class ConsoleGui
+    {
+        public static void SetPosition(int left, int top, bool clear = false)
+        {
+            if (clear)
+                Console.Clear();
+
+            Console.SetCursorPosition(left, top);
+        }
+
+        public static void Wait(ConsoleColor color = ConsoleColor.Red)
+        {
+            Console.SetCursorPosition(46, 28);
+            WriteLineColor("Нажмите что бы продолжить...", color, true);
+            Console.ReadKey();
+        }
+
+        public static void WriteLineColor(string str, ConsoleColor color, bool formatter = false)
+        {
+            if (formatter)
+                Console.SetCursorPosition((120 - str.Length) / 2, Console.CursorTop);
+
+            Console.ForegroundColor = color;
+            Console.WriteLine(str);
+            Console.ResetColor();
+
+        }
+
+
+        public static void WriteColor(string str, ConsoleColor color, bool formatter = false)
+        {
+            if (formatter)
+                Console.SetCursorPosition((120 - str.Length) / 2, Console.CursorTop);
+
+            Console.ForegroundColor = color;
+            Console.Write(str);
+            Console.ResetColor();
+        }
+    }
+}
