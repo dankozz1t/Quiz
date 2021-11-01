3c1969c0c5e299507deba1a350ddc5eec997caff
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-01 12:54:44 +0200

    refac

---
+++ DataBase/QuizPlayerDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace QuizGame
+{
+    public class QuizPlayerDatabase
+    {
+        private QuizPlayerDatabase() { }
+
+        private static List<QuizPlayer> quizPlayers;
+
+        public static List<QuizPlayer> GetQuizPlayer()
+        {
+            if (quizPlayers == null)
+                quizPlayers = new List<QuizPlayer>();
+            return quizPlayers;
+        }
+
+        public static void AddQuizPlayer(QuizPlayer user)
+        {
+            quizPlayers.Add(user);
+        }
+
+    }
+}
