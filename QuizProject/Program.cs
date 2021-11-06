8d24f6631cfabd91964983af9ceb78293fb0fc31
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-06 23:00:42 +0200

    main menu

---
+++ QuizProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuizGame;
+using QuizEditor;
+
+namespace QuizProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.Title = "МЕНЮ";
+
+            GlobalMenu menu = new GlobalMenu();
+            menu.Start();
+        }
+    }
+}
