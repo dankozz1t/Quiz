8e973bdc2b6f8c04dbeea4d122c2a307eb1e733c
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2021-11-06 23:05:45 +0200

    create complete quiz

--- src/QuizPlayer.cs
+++ src/QuizPlayer.cs
@@ -4,35 +4,63 @@ using NLog;
 
 namespace QuizGame
 {
+    [Serializable]
+    public class CompleteQuiz
+    {
+        public Quiz PassedQuiz;
+        public DateTime DateComplete;
+        public int Points;
+
+        public CompleteQuiz() { }
+        public CompleteQuiz(Quiz passedQuiz, int points, DateTime dateComplete)
+        {
+            PassedQuiz = passedQuiz;
+            Points = points;
+            DateComplete = dateComplete;
+        }
+    }
+
+
+    [Serializable]
     public class QuizPlayer : User
     {
-        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
-        private List<Quiz> passedQuizzes = new List<Quiz>();
-        public List<int> points = new List<int>();
-        public int totalPoints;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        public QuizPlayer(string login, string password, string name, DateTime birthDay, Access access) : base(login, password, name, birthDay, access)
-        { }
+        public List<CompleteQuiz> PassedQuizzes = new List<CompleteQuiz>();
 
-        public void Show()
+        public int TotalPoints;
+
+        public QuizPlayer() { }
+        public QuizPlayer(string login, string password, string name, DateTime birthDay, Access access)
+        : base(login, password, name, birthDay, access) { }
+
+        public void Show(bool quizInfo = true)
         {
-            Console.WriteLine(base.ToString());
-            Console.WriteLine($"Выполненных викторин {passedQuizzes.Count} :");
+            ConsoleGui.SetPosition(55, Console.CursorTop + 1);
+            ConsoleGui.WriteLineColor($"*USER: {Login}*--------------------------------------------------------------------------------------", ConsoleColor.Cyan, true);
+            ConsoleGui.SetPosition(55, Console.CursorTop);
+            ConsoleGui.WriteLineColor(base.ToString(), ConsoleColor.Cyan, true);
+            ConsoleGui.SetPosition(55, Console.CursorTop);
+            ConsoleGui.WriteLineColor($"Всего баллов: {TotalPoints} | Выполненных викторин {PassedQuizzes.Count} :", ConsoleColor.Green, true);
 
-            for (int i = 0; i < passedQuizzes.Count; i++)
+            if (quizInfo)
             {
-                Console.WriteLine(passedQuizzes[i]);
-                Console.WriteLine($"Баллы: {points[i]} / {passedQuizzes[i].questions.Count}");
+                for (int i = 0; i < PassedQuizzes.Count; i++)
+                {
+                    ConsoleGui.WriteLineColor("-------------------------------------------------------------", ConsoleColor.Cyan, true);
+                    PassedQuizzes[i].PassedQuiz.Show();
+                    ConsoleGui.WriteLineColor($"Баллы: {PassedQuizzes[i].Points} / {PassedQuizzes[i].PassedQuiz.questions.Count}   - {PassedQuizzes[i].DateComplete}", ConsoleColor.Yellow, true);
+                }
+
             }
         }
 
         private void CompletedQuiz(Quiz quiz, int points)
         {
-            passedQuizzes.Add(quiz);
-            this.points.Add(points);
-            totalPoints += points;
-            logger.Info($" Завершил Викторину \"{quiz.field}\" Набрав {points} / {quiz.questions.Count} баллов. | Login: {Login}");
-
+            PassedQuizzes.Add(new CompleteQuiz(quiz, points, DateTime.Now));
+    
+            TotalPoints += points;
+            Logger.Info($" Завершил Викторину \"{quiz.field}\" Набрав {points} / {quiz.questions.Count} баллов. | Login: {Login}");
         }
 
         private string[] FillMenu(int count, List<Answer> answers) //Заполняет массив стрингов ответами для меню
@@ -48,31 +76,31 @@ namespace QuizGame
             return menu;
         }
 
-        public void Play(Quiz quiz)
+        public int Play(Quiz quiz)
         {
             ConsoleGui.SetPosition(48, 1, true);
-            ConsoleGui.WriteColor("ВИКТОРИНА: " + quiz.field, ConsoleColor.Cyan, true);
-            ConsoleGui.WriteColor("Количество вопросов: " + quiz.questions.Count, ConsoleColor.Yellow, true);
-
+            ConsoleGui.WriteLineColor("ВИКТОРИНА: " + quiz.field, ConsoleColor.Cyan, true);
+            ConsoleGui.WriteLineColor("Количество вопросов: " + quiz.questions.Count, ConsoleColor.Yellow, true);
+            ConsoleGui.Wait();
 
             int scoresThisQuiz = 0;
 
             for (int i = 0; i < quiz.questions.Count; i++) //По всем вопросам
             {
-                Console.Clear();
                 int points = 0;
                 int positionAnswer = 0;
                 var countAnswers = quiz.questions[i].answers.Count;
 
                 string[] menu = FillMenu(countAnswers, quiz.questions[i].answers);
 
-                ConsoleGui.SetPosition(48, 3 + i);
+                ConsoleGui.SetPosition(48, 5, true);
                 ConsoleGui.WriteLineColor(" [?] " + quiz.questions[i].question, ConsoleColor.Yellow, true);
 
                 List<int> selectedAnswers = new List<int>();
+
                 while (positionAnswer != countAnswers) //Пока не нажата кнопка ОТПРАВИТЬ 
                 {
-                    ConsoleGui.SetPosition(55, 5 + i);
+                    ConsoleGui.SetPosition(55, 7);
                     positionAnswer = Menu.VerticalMenu(menu, true);
 
                     bool unique = false;
@@ -81,11 +109,11 @@ namespace QuizGame
                     else
                         unique = !(selectedAnswers.Contains(positionAnswer));
 
-                    if (unique)
+                    if (unique) //Если Нажатие на ответ УНИКАЛЬНОЕ
                     {
                         selectedAnswers.Add(positionAnswer);
                         menu[positionAnswer] += "*";
-                        if (positionAnswer != countAnswers) //Если не нажата кнопка ОТПРАВИТЬ
+                        if (positionAnswer != countAnswers)
                         {
                             if (quiz.questions[i].answers[positionAnswer].IsRight)
                                 points++;
@@ -98,7 +126,7 @@ namespace QuizGame
                         selectedAnswers.Remove(positionAnswer);
                         menu[positionAnswer] = menu[positionAnswer].Replace("*", "");
 
-                        if (positionAnswer != countAnswers) //Если не нажата кнопка ОТПРАВИТЬ
+                        if (positionAnswer != countAnswers)
                         {
                             if (quiz.questions[i].answers[positionAnswer].IsRight)
                                 points--;
@@ -106,7 +134,8 @@ namespace QuizGame
                                 points++;
                         }
                     }
-                    
+
+                    //---------------------Для тестирования
                     ConsoleGui.SetPosition(55, 27);
                     Console.WriteLine($"                                                  ");
                     ConsoleGui.SetPosition(55, 27);
@@ -115,18 +144,11 @@ namespace QuizGame
 
                 if (points == quiz.questions[i].CountCorrectAnswers())
                 {
-                    Console.WriteLine(" Все ответы правильные + 1 балл ");
-                    Console.WriteLine($" {points} / {quiz.questions[i].CountCorrectAnswers()}");
                     scoresThisQuiz++;
                 }
-                else
-                {
-                    Console.WriteLine("хзхзхз");
-                    Console.WriteLine($" {points} / {quiz.questions[i].CountCorrectAnswers()}");
-                }
             }
-
             CompletedQuiz(quiz, scoresThisQuiz);
+            return scoresThisQuiz;
         }
     }
 }
